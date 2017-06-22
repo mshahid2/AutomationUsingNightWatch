73af11a2f511b87e31529d16d39026411eea8614
Shahid <Owner@10.0.0.19>
2017-06-22 17:04:17 -0400

    comment changes.

--- AutomationUsingNightWatch/README.md
+++ AutomationUsingNightWatch/README.md
@@ -7,11 +7,12 @@ This project is Node.js based End-to-End (E2E) testing solution for browser base
 1. Install Node if you don't have in your system.
 2. Install nightwatch using command
 
-$ npm install [-g] nightwatch --save-dev
-Add -g option to make nightwatch runner available globally in your system.
+	$ npm install [-g] nightwatch --save-dev
+	Add -g option to make nightwatch runner available globally in your system.
 
-3.Install selenium
-npm install selenium-download --save-dev
+3.Install selenium using command
+
+	npm install selenium-download --save-dev
 
 ## Running end-to-end tests
 
--- AutomationUsingNightWatch/test/e2eTestCases/googleTest.js
+++ AutomationUsingNightWatch/test/e2eTestCases/googleTest.js
@@ -1,20 +1,18 @@
 var config = require('../../nightwatch.conf.js');
 module.exports = {
-		afterEach : function(browser, done) {
-		  browser.sessionLog(function(result) {
-		    console.log(result);
-		  }).end(function() {
-		    done();
-		  });
-		},
-		  'Test Google' : function (browser) {
-		    browser
-		      .maximizeWindow() // If on Linux set window size, max 1920x1080, like .resizeWindow(1920, 1080);
-		      .url('http://www.google.com/ncr')
-		      .waitForElementVisible('body', 1000)
-		      .setValue('input[type=text]', 'xavient')
-		      .waitForElementVisible('button[name=btnG]', 1000)
-		      .click('button[name=btnG]')
-		      .pause(10000);
-		  }
-		};
+	afterEach : function(browser, done) {
+		browser.sessionLog(function(result) {
+			console.log(result);
+		}).end(function() {
+			done();
+		});
+	},
+	'Test Google' : function(browser) {
+		browser.maximizeWindow().url('http://www.google.com/ncr')
+				.waitForElementVisible('body', 1000)
+				.setValue('input[type=text]', 'xavient')
+				.waitForElementVisible('button[name=btnG]', 1000)
+				.click('button[name=btnG]')
+				.pause(10000);
+	}
+};
